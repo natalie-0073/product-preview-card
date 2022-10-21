8f9782513e5a921a9eba991aadab0d30956e736e
Nataliia Chala <hatalochka.rv@gmail.com>
2022-10-21 01:32:08 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,8 +13,8 @@ function App() {
           <p className="product__comment">PERFUME</p>
           <h2 className="product__owner">Gabrielle Essense Eau De Parfum</h2>
         <p className="product__description">A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer Creator for the House of CHANEL.</p>
-        <div className="product__price">$149.99 <span className="product__price--old">$169.99</span></div>
-        <div className="product__buy" href="/"><img src={cart} alt="shopping cart" className="cart img-fluid"/><span>Add to Cart</span></div>
+        <div className="product__price">$149.99<span className="product__price--old">$169.99</span></div>
+        <div className="product__buy" href="/"><img src={cart} alt="shopping cart" className="cart img-fluid"/><span>Add to Cart</span></div>        
         </div>
       </div>
       </div>
